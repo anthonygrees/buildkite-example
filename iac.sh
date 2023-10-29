a667172691bf8d6c12d77b5d0dfa86ed65522fe8
Anthony Rees <anthony_rees01@hotmail.com>
2023-10-30 08:57:48 +1100

    Update iac.sh

--- iac.sh
+++ iac.sh
@@ -3,6 +3,11 @@ set -eo pipefail
 echo "--- :package: IAC Scan"
 
 pwd
+echo "--- :package: Command: ls -la"
 ls -la
+echo "--- :package: Command: lacework --help"
+lacework --help
+echo "--- :package: Command: lacework policy list --profile reesy1"
 lacework policy list --profile reesy1
+echo "--- :package: Command: lacework sca scan --save-results --profile reesy1 ."
 lacework sca scan --save-results --profile reesy1 .
